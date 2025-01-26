bd4e58d4faddf4ce7acf5aea0bba9406b300ed82
sattamalmuwallad <sattamalmuwallad@gmail.com>
2025-01-26 20:27:27 +0000

    Updated Movie Controller

--- GitHub projects/movie-watchlist/src/main/java/com/moviewatchlist/web/MovieController.java
+++ GitHub projects/movie-watchlist/src/main/java/com/moviewatchlist/web/MovieController.java
@@ -2,6 +2,7 @@ package com.moviewatchlist.web;
 
 import com.moviewatchlist.domain.Movie;
 import com.moviewatchlist.repository.MovieRepository;
+import com.moviewatchlist.service.MovieService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,21 +11,26 @@ import java.util.List;
 @RequestMapping("/api/movies")
 public class MovieController {
 
-
+    private final MovieService movieService;
     private final MovieRepository movieRepository;
 
-    public MovieController(MovieRepository movieRepository){
+    public MovieController(MovieService movieService, MovieRepository movieRepository){
+        this.movieService = movieService;
         this.movieRepository = movieRepository;
+    }
+    @GetMapping("/api/movies/{id}")
+    public Movie getMovieById(@PathVariable Long id) {
+        return movieService.findMovieById(id);
     }
 
     @GetMapping
     public List<Movie> getAllMovies(){
-        return movieRepository.findAll();
+        return movieService.getAllMovies();
     }
 
     @PostMapping
     public Movie addMovie(@RequestBody Movie movie){
-        return movieRepository.save(movie);
+        return movieService.saveMovie(movie);
     }
 
 }
