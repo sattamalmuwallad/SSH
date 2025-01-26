fca4081bea05a20f2b9245b6dd63c53a2ae28b5e
sattamalmuwallad <sattamalmuwallad@gmail.com>
2025-01-26 22:26:14 +0000

    Fixed bugs

--- GitHub projects/movie-watchlist/src/main/java/com/moviewatchlist/service/MovieService.java
+++ GitHub projects/movie-watchlist/src/main/java/com/moviewatchlist/service/MovieService.java
@@ -1,53 +1,63 @@
 package com.moviewatchlist.service;
 
+
 import com.moviewatchlist.domain.Movie;
 import com.moviewatchlist.repository.MovieRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
+
 
 @Service
 public class MovieService {
 
     private final MovieRepository movieRepository;
 
-    public MovieService(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
-    }
+     public MovieService(MovieRepository movieRepository){
+         this.movieRepository = movieRepository;
+     }
 
-    public Movie saveMovie(Movie movie) {
-        return movieRepository.save(movie);
-    }
+     public Movie saveMovie(Movie movie){
+         return movieRepository.save(movie);
+     }
 
-    public List<Movie> getAllMovies() {
+
+    public List<Movie> getAllMovies(){
         return movieRepository.findAll();
     }
-
     public boolean isMoviePresent(Long id) {
         return movieRepository.existsById(id);
     }
 
     public Movie findMovieById(Long id) {
         Optional<Movie> movie = movieRepository.findById(id);
-        if (movie.isPresent()) {
-            return movie.get();
-        } else {
-            throw new RuntimeException("We couldn't find the movie you're looking for with ID: " + id);
-        }
-    }
+         if (isMoviePresent(id)){
+             return movieRepository.findById(id).get();
+         } else {
+             throw new RuntimeException("We couldn't find the movie your lookig for"+ id);
+         }
 
+    }
     public Movie updateMovie(Long id, Movie updatedMovie) {
         if (!movieRepository.existsById(id)) {
             throw new RuntimeException("Movie not found with ID: " + id);
         }
 
         Movie existingMovie = movieRepository.findById(id).get();
-
         existingMovie.setTitle(updatedMovie.getTitle());
-        existingMovie.setGenre(updatedMovie.getGenre());
-        existingMovie.setReleaseDate(updatedMovie.getReleaseDate());
-
+        existingMovie.setDirector(updatedMovie.getDirector());
+        existingMovie.setReleaseYear(updatedMovie.getReleaseYear());
         return movieRepository.save(existingMovie);
     }
+
+    public void deleteMovie(Long id) {
+        if (!movieRepository.existsById(id)) {
+            throw new RuntimeException("Movie not found with ID: " + id);
+        }
+        movieRepository.deleteById(id);
+    }
+
+
 }
--- GitHub projects/movie-watchlist/src/main/java/com/moviewatchlist/web/MovieController.java
+++ GitHub projects/movie-watchlist/src/main/java/com/moviewatchlist/web/MovieController.java
@@ -1,6 +1,7 @@
 package com.moviewatchlist.web;
 
 import com.moviewatchlist.domain.Movie;
+import com.moviewatchlist.repository.MovieRepository;
 import com.moviewatchlist.service.MovieService;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +13,24 @@ public class MovieController {
 
     private final MovieService movieService;
 
+
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
-
-    @GetMapping("/{id}")
+    @GetMapping("/api/movies/{id}")
     public Movie getMovieById(@PathVariable Long id) {
         return movieService.findMovieById(id);
+    }
+
+
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        return movieService.getAllMovies();
+    }
+
+    @PostMapping
+    public Movie addMovie(@RequestBody Movie movie){
+        return movieService.saveMovie(movie);
     }
 
     @PutMapping("/{id}")
@@ -26,13 +38,11 @@ public class MovieController {
         return movieService.updateMovie(id, movie);
     }
 
-    @GetMapping
-    public List<Movie> getAllMovies() {
-        return movieService.getAllMovies();
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
     }
 
-    @PostMapping
-    public Movie addMovie(@RequestBody Movie movie) {
-        return movieService.saveMovie(movie);
-    }
+
+
 }
